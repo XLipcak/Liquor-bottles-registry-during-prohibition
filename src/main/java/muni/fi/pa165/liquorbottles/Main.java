92b061ae03a3e2444a0360d41fd40b70028d7ea1
Kubo <Kubo@Kubo-PC>
2014-10-04 12:16:58 +0200

    Project created.

---
+++ src/main/java/muni/fi/pa165/liquorbottles/Main.java
@@ -0,0 +1,11 @@
+package muni.fi.pa165.liquorbottles;
+
+/**
+ *
+ * @author Jakub Lipcak, Masaryk University
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
