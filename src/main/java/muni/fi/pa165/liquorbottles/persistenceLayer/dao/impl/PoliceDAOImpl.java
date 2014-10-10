929c4efe6625de788773007f5fde3de9f389a511
Kubo <Kubo@Kubo-PC>
2014-10-10 15:26:25 +0200

    Impl of DAO for: User, Police.

--- src/main/java/muni/fi/pa165/liquorbottles/persistenceLayer/dao/impl/PoliceDAOImpl.java
+++ src/main/java/muni/fi/pa165/liquorbottles/persistenceLayer/dao/impl/PoliceDAOImpl.java
@@ -1,53 +1,143 @@
 package muni.fi.pa165.liquorbottles.persistenceLayer.dao.impl;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
 import muni.fi.pa165.liquorbottles.persistenceLayer.dao.PoliceDAO;
+import muni.fi.pa165.liquorbottles.persistenceLayer.dao.UserDAO;
 import muni.fi.pa165.liquorbottles.persistenceLayer.entities.Police;
 
 /**
  *
- * @author Matus Novak, Masaryk University
+ * @author Jakub Lipcak, Masaryk University
  */
-public class PoliceDAOImpl implements PoliceDAO{
+public class PoliceDAOImpl implements PoliceDAO {
+
+    EntityManagerFactory emf;
+    UserDAO userDAO;
+
+    public PoliceDAOImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+        userDAO = new UserDAOImpl(emf);
+    }
 
     @Override
     public List<Police> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Police> allPoliceQuery = em.createQuery("SELECT p FROM Police p", Police.class);
+            List<Police> allPolice = allPoliceQuery.getResultList();
+            em.getTransaction().commit();
+
+            return allPolice;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Police findById(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            Police police = em.find(Police.class, id);
+            em.getTransaction().commit();
+
+            return police;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Police findByUsername(String userName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Police> policeByUserNameQuery = em.createQuery("SELECT p FROM Police p "
+                    + "WHERE p.username='" + userName + "'", Police.class);
+            Police police = policeByUserNameQuery.getSingleResult();
+            em.getTransaction().commit();
+
+            return police;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Police findByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Police> policeByNameQuery = em.createQuery("SELECT p FROM Police p "
+                    + "WHERE p.name='" + name + "'", Police.class);
+            Police police = policeByNameQuery.getSingleResult();
+            em.getTransaction().commit();
+
+            return police;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
-    public Police findAddress(String address) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Police findByAddress(String address) {
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Police> policeByAddress = em.createQuery("SELECT p FROM Police p "
+                    + "WHERE p.address='" + address + "'", Police.class);
+            Police police = policeByAddress.getSingleResult();
+            em.getTransaction().commit();
+
+            return police;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public void insertPolice(Police police) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        userDAO.insertUser(police);
     }
 
     @Override
     public void updatePolice(Police police) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        userDAO.updateUser(police);
     }
 
     @Override
     public void deletePolice(Police police) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        userDAO.deleteUser(police);
     }
-    
+
 }
--- src/main/java/muni/fi/pa165/liquorbottles/persistenceLayer/dao/impl/UserDAOImpl.java
+++ src/main/java/muni/fi/pa165/liquorbottles/persistenceLayer/dao/impl/UserDAOImpl.java
@@ -1,38 +1,157 @@
 package muni.fi.pa165.liquorbottles.persistenceLayer.dao.impl;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
 import muni.fi.pa165.liquorbottles.persistenceLayer.dao.UserDAO;
 import muni.fi.pa165.liquorbottles.persistenceLayer.entities.User;
 
 /**
  *
- * @author Matus Novak, Masaryk University
+ * @author Jakub Lipcak, Masaryk University
  */
-public class UserDAOImpl implements UserDAO{
+public class UserDAOImpl implements UserDAO {
+
+    EntityManagerFactory emf;
+
+    public UserDAOImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
 
     @Override
     public List<User> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            TypedQuery<User> allUsersQuery = em.createQuery("SELECT u FROM User u", User.class);
+            List<User> allUsers = allUsersQuery.getResultList();
+            em.getTransaction().commit();
+
+            return allUsers;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public User findById(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            User user = em.find(User.class, id);
+            em.getTransaction().commit();
+
+            return user;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public User findByUsername(String userName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            TypedQuery<User> userByUserNameQuery = em.createQuery("SELECT u FROM User u "
+                    + "WHERE u.username='" + userName + "'", User.class);
+            User user = userByUserNameQuery.getSingleResult();
+            em.getTransaction().commit();
+
+            return user;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
-    public User findPassByUsername(String userName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String findPassByUsername(String userName) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            TypedQuery<String> passByNameQuery = em.createQuery("SELECT u.password FROM User u "
+                    + "WHERE u.username='" + userName + "'", String.class);
+            String password = passByNameQuery.getSingleResult();
+            em.getTransaction().commit();
+
+            return password;
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
-    public void deleteById(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void insertUser(User user) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
-    
+
+    @Override
+    public void deleteUser(User user) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.remove(em.contains(user) ? user : em.merge(user));
+            em.getTransaction().commit();
+
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    @Override
+    public void updateUser(User user) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.merge(user);
+            em.getTransaction().commit();
+
+        } catch (Exception ex) {
+            throw new PersistenceException("Transaction failed. \n" + ex.getMessage(), ex);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 }
