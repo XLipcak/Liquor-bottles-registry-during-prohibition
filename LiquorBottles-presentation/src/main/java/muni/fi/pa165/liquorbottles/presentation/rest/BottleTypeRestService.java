8f27ef114b9ec23ef51e600739ecb14d82c7f8d9
Michal Štora <stora.michal@gmail.com>
2014-12-18 18:11:00 +0100

    Rest for bottleType

---
+++ LiquorBottles-presentation/src/main/java/muni/fi/pa165/liquorbottles/presentation/rest/BottleTypeRestService.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muni.fi.pa165.liquorbottles.presentation.rest;
+
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import muni.fi.pa165.liquorbottles.api.dto.BottleTypeDTO;
+import muni.fi.pa165.liquorbottles.api.services.BottleTypeService;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+
+/**
+ *
+ * @author Michal Štora, Masaryk University
+ */
+@Path("/bottleType")
+public class BottleTypeRestService {
+    
+    @SpringBean
+    private BottleTypeService bottleTypeService;
+    
+    private BottleTypeDTO bottleType;
+    
+    @GET
+    @Path("/id/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBottleTypeById(@PathParam("param") long id) {
+        bottleType = bottleTypeService.findById(id);
+        if (bottleType != null) {
+            return Response.status(Response.Status.OK).entity(bottleType).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllBottleTypes() {
+        List<BottleTypeDTO> bottles = bottleTypeService.findAll();
+        if (bottles != null) {
+            return Response.status(Response.Status.OK).entity(bottles).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Path("/alcType/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBottleTypeByAlcType(@PathParam("param") String AlcType) {
+        List<BottleTypeDTO> bottles = bottleTypeService.findByAlcType(AlcType);
+        if (bottles != null) {
+            return Response.status(Response.Status.OK).entity(bottles).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Path("/power/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBottleTypeByPower(@PathParam("param") int power) {
+        List<BottleTypeDTO> bottles = bottleTypeService.findByPower(power);
+        if (bottles != null) {
+            return Response.status(Response.Status.OK).entity(bottles).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Path("/volume/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBottleTypeByVolume(@PathParam("param") int volume) {
+        List<BottleTypeDTO> bottles = bottleTypeService.findByVolume(volume);
+        if (bottles != null) {
+            return Response.status(Response.Status.OK).entity(bottles).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Path("/producer/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBottleTypeByAlcType(@PathParam("param") long producerId) {
+        List<BottleTypeDTO> bottles = bottleTypeService.findByProducer(producerId);
+        if (bottles != null) {
+            return Response.status(Response.Status.OK).entity(bottles).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void update(BottleTypeDTO toUpdate) {
+        bottleTypeService.updateBottleType(bottleType);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void add(BottleTypeDTO toAdd) {
+        bottleTypeService.insertBottleType(bottleType);
+    }
+    
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void remove(BottleTypeDTO toDelete) {
+        bottleTypeService.deleteBottleType(bottleType);
+    }
+}
